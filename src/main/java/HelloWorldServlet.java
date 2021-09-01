91daa282bdca1e46888c8d29a54421da904923dc
calaisgalbraith <calaisgalbraith@gmail.com>
2021-09-01 10:15:21 -0500

    refactored code

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,21 +3,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+//DONE Create a page that displays "Hello World!" when a user visits /hello
+//Make the page say "Hello, <name>!" if name is passed as part of the query string
+// (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
 
-@WebServlet(urlPatterns = "/hello")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
 
-        //DONE Create a page that displays "Hello World!" when a user visits /hello
-        //Make the page say "Hello, <name>!" if name is passed as part of the query string
-        // (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
         String name = req.getParameter("name");
 
         if ((name == null)) {
-            res.getWriter().println("Hello World!");
+            out.println("Hello World!");
         } else {
-            res.getWriter().printf("Hello %s!", name);
+            out.printf("Hello %s!", name);
         }
 
     }
